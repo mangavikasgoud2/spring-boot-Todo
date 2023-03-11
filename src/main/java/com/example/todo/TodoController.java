37007d6ac658bc51f73f704a23f2fa43207285fe
MANGA VIKAS GOUD <126855039+mangavikasgoud2@users.noreply.github.com>
2023-03-11 12:51:42 +0530

    Update TodoController.java

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -1,12 +1,4 @@
-/*
- * You can use the following import statements
- * 
- * import org.springframework.web.bind.annotation.*;
- * import java.util.*;
- * 
- */
 
-// Write your code here
 package com.example.todo;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
